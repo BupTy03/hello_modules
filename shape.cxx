4afe80f4c59d0a833b7836d3279a54fc4c73818c
BupTy03 <anton.lagosha.99@mail.ru>
2023-10-20 22:40:11 +0500

    using template lambdas

--- shape.cxx
+++ shape.cxx
@@ -10,27 +10,6 @@ class ShapeImpl;
 
 }
 
-namespace detail {
-
-struct ShapePrintVisitor
-{
-   void operator()(std::monostate) { throw std::runtime_error("Nothing to print!"); }
-
-   template<typename T>
-   void operator()(T&& v) { v.Print(); }
-};
-
-struct ShapeNameVisitor
-{
-   std::string_view operator()(std::monostate) { return {}; }
-
-   template<typename T>
-   std::string_view operator()(T&& v) { return v.Name(); }
-};
-
-}
-
-
 template<typename... Types>
 class ShapeImpl
 {
@@ -40,9 +19,9 @@ public:
    template<typename S>
    explicit ShapeImpl(S s) : impl_(std::move(s)) {}
 
-   void Print() const { std::visit(detail::ShapePrintVisitor(), impl_); }
-   std::string_view Name() const { return std::visit(detail::ShapeNameVisitor(), impl_); }
+   void Print() const { std::visit([](auto&& s) { s.Print(); }, impl_); }
+   std::string_view Name() const { return std::visit([](auto&& s) { return s.Name(); }, impl_); }
 
 private:
-   std::variant<std::monostate, Types...> impl_;
+   std::variant<Types...> impl_;
 };
